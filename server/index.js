e1c1bce8dc1a2d9bdeab07c94e1837a14630bebe
yellowpunch4 <yellowpunch4@ya.ru>
2025-05-22 17:59:11 +0300

    feat[server]: added  offers controller stuff

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import * as dotenv from 'dotenv';
 import sequelize from './config/database.js';
 import router from './routes/index.js';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
+import { fileURLToPath } from 'url';
+
 // import './models/offer.js'
 // import './models/review.js'
 // import './models/user.js'
@@ -10,13 +14,18 @@ import router from './routes/index.js';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
 app.use('/', router);
+app.use(errorMiddleware);
+app.use('/static', express.static(path.resolve(__dirname,'static')));
 
 app.get('/', (req,res) => {
     res.status(200).json({message: 'Ура! Все заработало'})
